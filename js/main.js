ff3f49c216d4b919b30e57600c5b8e56698bb2f0
Ryan Arana <ryan.arana@gmail.com>
2012-04-08 21:16:53 -0700

    Level loading partially complete.

--- js/main.js
+++ js/main.js
@@ -8,12 +8,21 @@ var SCREEN_W = 800;
 var SCREEN_H = 600;
 var FRAME_W = 2;
 var V_PAD = 6;
-var LVL_H = 400;
-var LVL_W = 400;
-var LVL_DATA_X = 23;
-var LVL_DATA_Y = 21;
+
+var lvl_width_px = 400;
+var lvl_height_px = 400;
+// Calc a size (in px) that is evenly divisible by the number of
+// tiles per row/col to avoid gaps in the grid.
+var TILE_COLS = 23;
+var TILE_ROWS = 21;
+var TILE_W = Math.ceil(lvl_width_px / TILE_COLS);
+var TILE_H = Math.ceil(lvl_height_px / TILE_ROWS);
+var LVL_W =  TILE_W * TILE_COLS;
+var LVL_H = TILE_H * TILE_ROWS;
+
 var STATUS_H = 75;
 var STATUS_W = LVL_W;
+
 var MSG_H = SCREEN_H - STATUS_H - LVL_H - (V_PAD * 4);
 var MSG_W = LVL_W;
 
@@ -49,13 +58,119 @@ gamejs.ready(function() {
 		return I;
 	};
 
-	function Tile(blocked, I) {
-		var I = I || {
-			blocked: blocked,
+	function Tile(data, rect, wall_color, floor_color) {
+		var I = {
+			blocked: false,
 			warp: false,
 			explored: false,
-			style: ""
+			data: data,
+			rect: rect
 		};
+
+		var _drawBlock = function() {
+			draw.rect(ctx, wall_color, rect, 0);
+		};
+		var _drawBlank = function() {
+			draw.rect(ctx, floor_color, rect, 0);
+		};
+		var _drawCageCornerTL = function() {
+		};
+		var _drawCageCornerTR = function() {
+		};
+		var _drawCageCornerBL = function() {
+		};
+		var _drawCageCornerBR = function() {
+		};
+		var _drawCageWallVert = function() {
+		};
+		var _drawCageWallHori = function() {
+		};
+		var _drawCageDoor = function () {
+		};
+		var _drawWarp = function() {
+		};
+
+		switch (data) {
+			default: {
+				I.draw = _drawBlank;
+				break;
+			}
+			case '1': {
+				I.blocked = true;
+				I.draw = _drawBlock;
+				break;
+			}
+			case 'w': {
+				I.blocked = false;
+				I.warp = true;
+				I.draw = _drawWarp;
+				break;
+			}
+			case 'l': {
+				I.blocked = true;
+				I.draw = _drawCageCornerTL;
+				break;
+			}
+			case 'r': {
+				I.blocked = true;
+				I.draw = _drawCageCornerTR;
+				break;
+			}
+			case 'L': {
+				I.blocked = true;
+				I.draw = _drawCageCornerBL;
+				break;
+			}
+			case 'R': {
+				I.blocked = true;
+				I.draw = _drawCageCornerBR;
+				break;
+			}
+			case '=': {
+				I.blocked = true;
+				I.draw = _drawCageWallHori;
+				break;
+			}
+			case 'v': {
+				I.blocked = true;
+				I.draw = _drawCageWallVert;
+				break;
+			}
+			case '-': {
+				I.blocked = true;
+				I.draw = _drawCageDoor;
+				break;
+			}
+			case '.': {
+				// TODO: Add Pellets
+				break;
+			}
+			case 'o': {
+				// TODO: Add Pellets
+				break;
+			}
+			case 'B': {
+				// TODO: Add Ghosts
+				break;
+			}
+			case 'P': {
+				// TODO: Add Ghosts
+				break;
+			}
+			case 'I': {
+				// TODO: Add Ghosts
+				break;
+			}
+			case 'A': {
+				// TODO: Add Ghosts
+				break;
+			}
+			case '@': {
+				// TODO: Add Player
+				break;
+			}
+		}
+
 		return I;
 	};
 
@@ -64,22 +179,13 @@ gamejs.ready(function() {
 			name: '',
 			dlvl: 0,
 			sublvl: 0,
-			map_data: [],
-			lvl_data: [],
+			tiles: [],
 			wall_color: '#f00',
 			floor_color: '#000',
 			dark: false
 		};
 
-		I.lvl_data = [];
-		for (var y=0; y < LVL_DATA_Y; y++) {
-			I.lvl_data.push([]);
-			for (var x=0; x < LVL_DATA_X; x++) {
-				I.lvl_data[y].push(new Tile(false));
-			}
-		}
-
-		I.setUp = function() {
+		I.load = function(data) {
 			if (this.dlvl >= 1 && this.dlvl <= 3) {
 				pelletColor = "#944";
 			} else if (this.dlvl >= 4 && this.dlvl <= 6) {
@@ -92,70 +198,28 @@ gamejs.ready(function() {
 				pelletColor = "rgb(216, 216, 252)";
 			}
 
-			for (var y=0; y < LVL_DATA_Y; y++) {
-				for (var x=0; x < LVL_DATA_X; x++) {
-					map_val = this.map_data[y][x];
-					lvl_val = this.lvl_data[y][x];
-					switch (map_val) {
-						case '1':
-							lvl_val.blocked = true;
-							break;
-						case 'w':
-							lvl_val.warp = true;
-							break;
-						case 'l':
-							lvl_val.blocked = true;
-							lvl_val.style = 'top-left-corner';
-							break;
-						case 'r':
-							lvl_val.blocked = true;
-							lvl_val.style = 'top-right-corner';
-							break;
-						case 'L':
-							lvl_val.blocked = true;
-							lvl_val.style = 'bot-left-corner';
-							break;
-						case 'R':
-							lvl_val.blocked = true;
-							lvl_val.style = 'bot-right-corner';
-							break;
-						case '=':
-							lvl_val.blocked = true;
-							lvl_val.style = 'horizontal-bar';
-							break;
-						case 'v':
-							lvl_val.blocked = true;
-							lvl_val.style = 'vertical-bar';
-							break;
-						case '-':
-							lvl_val.blocked = true;
-							lvl_val.style = 'cage-door';
-							break;
-						case '.':
-							// TODO: Add Pellets
-							break;
-						case 'o':
-							// TODO: Add Pellets
-							break;
-						case 'B':
-							// TODO: Add Ghosts
-							break;
-						case 'P':
-							// TODO: Add Ghosts
-							break;
-						case 'I':
-							// TODO: Add Ghosts
-							break;
-						case 'A':
-							// TODO: Add Ghosts
-							break;
-						case '@':
-							// TODO: Add Player
-							break;
-					}
+			for (var y=0; y < TILE_ROWS; y++) {
+				var yOffset = y * TILE_H + lvlPanel.rect.top;
+				for (var x=0; x < TILE_COLS; x++) {
+					var xOffset = x * TILE_W + lvlPanel.rect.left;
+					var rect = new gamejs.Rect([xOffset, yOffset], 
+											   [TILE_W, TILE_H]);
+					var tile = new Tile(data[y][x], rect, this.wall_color, this.floor_color);
+					this.tiles.push(tile);
 				}
 			}
 		};
+
+		I.draw = function() {
+			if (this.dark) {
+			} else {
+				for (var i=0; i<this.tiles.length; i++) {
+					if (this.tiles[i].draw) {
+						this.tiles[i].draw();
+					}
+				}
+			}
+		}
 
 		return I;
 	};
@@ -191,10 +255,14 @@ gamejs.ready(function() {
 		lvlTxt = f.render("The Level/Map/Game will be rendered here", "#ccffff");
 		msgTxt = f.render("Game Messages will be printed here", "cccccc");
 
+		var lvl = new Level();
+		lvl.load(level_7_9);
+		lvl.draw();
+
 		ctx.blit(statusTxt, [statusPanel.center().x - (statusTxt.rect.width/2),
 				 statusPanel.center().y - (statusTxt.rect.height/2)]);
-		ctx.blit(lvlTxt, [lvlPanel.center().x - (lvlTxt.rect.width/2), 
-				 lvlPanel.center().y - (lvlTxt.rect.height/2)]);
+		//ctx.blit(lvlTxt, [lvlPanel.center().x - (lvlTxt.rect.width/2), 
+		//		 lvlPanel.center().y - (lvlTxt.rect.height/2)]);
 		ctx.blit(msgTxt, [msgPanel.center().x - (msgTxt.rect.width/2),
 				 msgPanel.center().y - (msgTxt.rect.height/2)]);
 	};
