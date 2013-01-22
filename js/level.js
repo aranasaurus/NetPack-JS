fd1129673cd73eb55f9d44be688c24510d250b2b
Ryan Arana <ryan.arana@gmail.com>
2013-01-22 01:05:01 -0800

    First stab at map loading

--- js/level.js
+++ js/level.js
@@ -1,140 +1,41 @@
-var tile = require("tile");
-var entities = require("entities");
+define(["jquery", "tile"], function($, tile) {
+    return {
+        Level: function (level_data, spec) {
+            var defaults = {
+                tile_cols: 23,
+                tile_rows: 21
+            };
+            spec = $.extend({}, defaults, spec);
 
-var Level = exports.Level = function() { 
-    this.name = '';
-    this.dlvl = 0;
-    this.sublvl = 0;
-    this.tiles = [];
-    this.ghosts = [];
-    this.pellets = [];
-    this.items = [];
-    this.wallColor = "#00f";
-    this.floorColor = "#000";
-    this.dark = false;
-    this.pelletColor = "#944";
+            this.tiles = [];
+            var data = level_data || [[]];
 
-    return this;
-};
-
-Level.prototype.getRect = function(entityOrTileIndex) {
-    var tileIndex = entityOrTileIndex.tileIndex || entityOrTileIndex;
-    return this.tiles[tileIndex[ROW]][tileIndex[COL]].rect;
-};
-
-Level.prototype.getTile = function(entityOrTileIndex) {
-    var tileIndex = entityOrTileIndex.tileIndex || entityOrTileIndex;
-    return this.tiles[tileIndex[ROW]][tileIndex[COL]];
-};
-
-Level._getObject = function(row, col, arr) {
-    for (var i=0; i<arr.length; i++) {
-        var o = arr[i];
-        if (o.tileIndex[ROW] == row && o.tileIndex[COL] == col) {
-            return o;
-        }
-    }
-    return false;
-};
-
-Level.prototype.getGhost = function(row, col) {
-    return Level._getObject(row, col, this.ghosts);
-};
-
-Level.prototype.getPellet = function(row, col) {
-    return Level._getObject(row, col, this.pellets);
-};
-
-Level.prototype.getItem = function(row, col) {
-    return Level._getObject(row, col, this.items);
-};
-
-Level.prototype.removePellet = function(pellet) {
-    var i = this.pellets.indexOf(pellet);
-    gamejs.info('Removing pellet[' + i + '] from pellets');
-    this.pellets.splice(i, 1);
-}
-
-Level.prototype.getTileAtXY = function(xy) {
-    var col = (xy[0] - lvlPanel.left) / TILE_W;
-    var row = (xy[1] - lvlPanel.top) / TILE_H;
-
-    if (col < TILE_COLS && row < TILE_ROWS) {
-        return this.tiles[row][col];
-    } else {
-        return false;
-    }
-};
-
-Level.prototype.draw = function() {
-    if (this.dark) {
-        // TODO: Implement Dark mode
-    } else {
-        var _draw = function(o) {
-            o.draw();
-        };
-        this.tiles.forEach(function(row) {
-            row.forEach(_draw);
-        });
-        this.pellets.forEach(_draw);
-        this.items.forEach(_draw);
-        this.ghosts.forEach(_draw);
-        this.player.draw();
-    }
-}
-
-Level.prototype.load = function(data) {
-    gamejs.info("Loading map data...");
-    if (this.dlvl >= 1 && this.dlvl <= 3) {
-        this.pelletColor = "#944";
-    } else if (this.dlvl >= 4 && this.dlvl <= 6) {
-        this.pelletColor = "rgb(216, 216, 252)";
-    } else if (this.dlvl >= 7 && this.dlvl <= 9) {
-        this.pelletColor = "#f00";
-    } else if (this.dlvl >= 10 && this.dlvl <= 12) {
-        this.pelletColor = "#0ff";
-    } else if (this.dlvl >= 13) {
-        this.pelletColor = "rgb(216, 216, 252)";
-    }
-
-    for (var row=0; row<TILE_ROWS; row++) {
-        this.tiles.push([]);
-        var yOffset = row * TILE_H + lvlPanel.rect.top;
-        for (var col=0; col<TILE_COLS; col++) {
-            var xOffset = col * TILE_W + lvlPanel.rect.left;
-            var rect = new gamejs.Rect([xOffset, yOffset], [TILE_W, TILE_H]);
-
-            var t = new tile.Tile(data[row][col], rect, this.wallColor, this.floorColor);
-            this.tiles[row].push(t);
-
-            switch (data[row][col]) {
-                // Ghosts
-                case 'B': case 'P': case 'I': case 'A': {
-                    gamejs.info("Adding Ghost (" + data[row][col] + ") at index [" + row + ", " + col + "]");
-                    this.ghosts.push(new entities.Ghost([row, col], data[row][col]));
-                    break;
-                }
-                case '@': {
-                    gamejs.info("Adding Player at index [" + row + ", " + col + "]");
-                    this.player = new entities.Player([row, col]);
-                    break;
-                }
-                case '.': {
-                    gamejs.info("Adding Pellet at index [" + row + ", " + col + "]");
-                    this.pellets.push(new entities.Pellet([row, col], '.'));
-                    break;
-                }
-                case 'o': {
-                    gamejs.info("Adding PowerPellet at index [" + row + ", " + col + "]");
-                    this.pellets.push(new entities.Pellet([row, col], 'o'));
-                    break;
-                }
-                default: {
-                    // Do nothing, handled by Tile
-                    break;
+            var $level = $('<table id="level" cellspacing="0" cellpadding="0"></table>');
+            $level.appendTo($('#gamePanel'));
+            var tile_width = Math.ceil($level.parent().width() / spec.tile_cols)
+            for (var row=0; row<spec.tile_rows; row++) {
+                this.tiles.push([]);
+                var $row = $('<tr></tr>');
+                $level.append($row);
+                for (var col=0; col<spec.tile_cols; col++) {
+                    var $tile = $('<td></td>');
+                    $tile.width(tile_width);
+                    $tile.height(tile_width);
+                    var t = new tile.Tile({ index: [row, col], type:data[row][col] });
+                    this.tiles[row].push(t);
+                    switch(t.data.type) {
+                        case "1":
+                            $tile.addClass('wall');
+                            break;
+                        default:
+                            break;
+                    }
+                    t.element = $tile;
+                    $row.append($tile);
                 }
             }
-        }
-    }
-};
 
+            return this;
+        }
+    };
+});
--- js/main.js
+++ js/main.js
@@ -1,128 +1,25 @@
-// All classes use these two, so store them in a global var 
-// so they're accessible everywhere.
-window.gamejs = require('gamejs');
-window.draw = require('gamejs/draw');
-var font = require('gamejs/font');
-
-var panel = require('panel');
-var tile = require('tile');
-var level = require('level');
-var entities = require('entities');
-
-gamejs.ready(function() {
-    gamejs.setLogLevel(0);
-
-    // TODO: these 4 are a lil stinky... I may want to find a
-    // cleaner way to do these. Especially the 2 frame ones.
-    window.ctx = gamejs.display.setMode([SCREEN_W, SCREEN_H]);
-    window.frameX = Math.floor((SCREEN_W - LVL_W) / 2);
-    window.frameColor = "#fcfcfc";
-    window.messages = [];
-    window.message = function(msg, color) {
-        window.messages.push([msg, color]);
-        gamejs.info(msg + " [" + color + "]");
-    };
-
-    var wrap = function(txt, f) {
-        var words = txt.split(' ');
-        var lines = ["", ""];
-        var wrapped = false;
-        words.forEach(function(word) {
-            if (wrapped || f.size(lines[0] + ' ' + word)[0] > (window.MSG_W - (window.MSG_PADDING * 2))) {
-                wrapped = true;
-                lines[1] = (lines[1] + ' ' + word).trim();
-            } else {
-                lines[0] = (lines[0] + ' ' + word).trim();
-            }
-        });
-        if (lines[1] == "") {
-            lines.splice(1);
-        } else {
-            lines[1] = '  ' + lines[1];
-        }
-        return lines;
-    };
-
-    function tick(msDuration) {
-
-        // Update stuff
-        var _update = function(o) {
-            o.update();
-        };
-        loadedLevel.ghosts.forEach(_update);
-        loadedLevel.items.forEach(_update);
-        loadedLevel.player.update();
-
-        // Draw stuff
-        ctx.fill("#000");
-        lvlPanel.draw();
-        loadedLevel.draw();
-        statusPanel.draw();
-        msgPanel.draw();
-
-        ctx.blit(statusTxt, [statusPanel.rect.center[0] - (statusTxt.rect.width/2), statusPanel.rect.center[1] - (statusTxt.rect.height/2)]);
-
-        // Write messages to game console
-        f = new font.Font("14px Verdana");
-        var actualMessageLines = [];
-        var actualMessageColors = {};
-        window.messages.forEach(function(m) {
-            var wrapped = wrap(m[0], f);
-            wrapped.forEach(function(w) {
-                actualMessageColors[w] = m[1];
-                actualMessageLines.push(w);
-            });
-        });
-        while (actualMessageLines.length > window.MSG_LINES) {
-            actualMessageLines.shift();
-        }
-        var y = msgPanel.rect.top + 2;
-        actualMessageLines.forEach(function(line) {
-            var msgTxt = f.render(line, actualMessageColors[line]);
-            ctx.blit(msgTxt, [msgPanel.rect.left + window.MSG_PADDING, y]);
-            y += msgTxt.rect.height + 2;
-        });
-
-        if (FPS != lastFPS) {
-            gamejs.info("Changing FPS from " + lastFPS + " to " + FPS);
-            gamejs.time.deleteCallback(tick, lastFPS);
-            gamejs.time.fpsCallback(tick, this, FPS);
-            lastFPS = FPS;
-        }
-    };
-
-    function init() {
-        draw.rect(ctx, "#000", new gamejs.Rect([0,0], [SCREEN_W, SCREEN_H]), 0);
-
-        // Set up the 3 main panels of the game
-        window.statusPanel = new panel.Panel({
-            name: "status", 
-            rect: new gamejs.Rect([frameX, V_PAD], [STATUS_W, STATUS_H]),
-            color: frameColor,
-            borderWidth: FRAME_W
-        });
-        window.lvlPanel = new panel.Panel({
-            name: "lvl", 
-            rect: new gamejs.Rect([frameX, statusPanel.rect.bottom + V_PAD], [LVL_W, LVL_H]),
-            color: "#000",
-            borderWidth: 0
-        });
-        window.msgPanel = new panel.Panel({
-            name: "messages", 
-            rect: new gamejs.Rect([frameX, lvlPanel.rect.bottom + V_PAD], [MSG_W, MSG_H]),
-            color: frameColor,
-            borderWidth: FRAME_W
-        });
-
-        f = new font.Font("14px Verdana");
-        window.statusTxt = f.render("Status Text (HP/XP) goes here.", "#cccccc");
-
-        window.loadedLevel = new level.Level();
-        loadedLevel.load(level_7_9);
-
-        window.lastFPS = FPS;
-        gamejs.time.fpsCallback(tick, this, FPS);
-    };
-
-    init();
+require(["jquery", "level"], function($, level) {
+    var loadedLevel = new level.Level([
+                                   '11111111111111111111111',
+                                   '11.....1.......1.....11',
+                                   '11o111.1.11111.1.111o11',
+                                   '11...................11',
+                                   '1111.1.111.1.111.1.1111',
+                                   'w   .1.....1.....1.   w',
+                                   '1111.111 11111 111.1111',
+                                   '1111....   B   ....1111',
+                                   '1111.111 l=-=r 111.1111',
+                                   '1111.1   vPIAv   1.1111',
+                                   '1111.1 1 L===R 1 1.1111',
+                                   'w   .  1       1  .   w',
+                                   '1111.11111 1 11111.1111',
+                                   '1111.....  1  .....1111',
+                                   '1111.111.11111.111.1111',
+                                   '11.........@.........11',
+                                   '11.111.111.1.111.111.11',
+                                   '11.111.1...1...1.111.11',
+                                   '11o111.1.11111.1.111o11',
+                                   '11...................11',
+                                   '11111111111111111111111'
+    ]);
 });
